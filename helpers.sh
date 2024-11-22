ef7c854fddf324f6dfad74ff6d2528fb83e5b287
Igor Khokhriakov <ingvord.mail@gmail.com>
2024-11-22 17:18:41 +0100

    Create helpers.sh

---
+++ helpers.sh
@@ -0,0 +1,7 @@
+#mongo
+docker run -d -p 27017:27017 --network root_elastic --name mongo mongodb/mongodb-community-server
+#backend
+docker build -t back .
+docker run --rm -d -p 33000:3000 -e SWAGGER_PATH=/backend/explorer -e MONGODB_URI=mongodb://db:27017/dacat --link mongo:db --link apm:apm --network root_elastic --name backend back
+#frontend
+docker build -t front .
